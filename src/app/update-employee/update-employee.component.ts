e8763a9284e39781a5757bf4de8d4ba4985e9ba1
Deepak Sankaran <sdeepak101@gmail.com>
2024-08-19 18:39:17 +0530

    changes

--- src/app/update-employee/update-employee.component.ts
+++ src/app/update-employee/update-employee.component.ts
@@ -10,26 +10,28 @@ import { Router } from '@angular/router';
   styleUrl: './update-employee.component.css'
 })
 export class UpdateEmployeeComponent {
-  id :number;
-  employee:Employee = new Employee();
-  constructor(private employeeService:EmployeeService,private route:ActivatedRoute,private router :Router){}
+ 
+   id: number;
+  employee: Employee = new Employee();
+  constructor(private employeeService: EmployeeService, private route: ActivatedRoute, private router: Router) { }
 
-  ngOnInit():void{
-    this.id=this.route.snapshot.params[`id`];
-    this.employeeService.getEmployeeById(this.id).subscribe(data =>{
-      this.employee = data;},
-      error =>console.log(error));
-    
-    }
-    getToEmployeeList() {
-      this.router.navigate([`/employees`])
-    }
-    onSubmit() {
-     this.employeeService.updateEmployee(this.id,this.employee).subscribe(data =>{
-this.getToEmployeeList();
-     },
-     error =>console.log(error));
-    
-    }
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params[`id`];
+    this.employeeService.getEmployeeById(this.id).subscribe(data => {
+      this.employee = data;
+    },
+      error => console.log(error));
+
   }
+  getToEmployeeList() {
+    this.router.navigate([`/employees`])
+  }
+  onSubmit() {
+    this.employeeService.updateEmployee(this.id, this.employee).subscribe(data => {
+      this.getToEmployeeList();
+    },
+      error => console.log(error));
+
+  }
+}
 
