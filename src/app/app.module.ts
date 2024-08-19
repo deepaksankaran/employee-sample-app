4e9a2516c5bc1e8362b59126c2483d941f88883f
Deepak Sankaran <sdeepak101@gmail.com>
2024-08-19 12:13:42 +0530

    employee creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,20 @@ import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { provideHttpClient } from '@angular/common/http';
 import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeListComponent,
-    CreateEmployeeComponent
+    CreateEmployeeComponent,
+    UpdateEmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
     
   ],
   providers: [provideHttpClient()], 
